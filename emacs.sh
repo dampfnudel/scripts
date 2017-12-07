c3b987b63e2897ac875fa396346473d711765d95
Markus Bayer <bayer_markus@web.de>
2017-12-07 15:11:27 +0100

    emacs app is always server

--- emacs.sh
+++ emacs.sh
@@ -1,94 +1,24 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-# emacs
-# MATCHES="$(ps aux | grep "Emacs --daemon" | wc -l)"
-# echo "${MATCHES: -1}"
-# if [[ "${MATCHES: -1}" == 1 ]]
-# then
-#     echo "starting server"
-#     emacs --daemon
-# fi
+function emacs_running () {
+    local IS_RUNNING=$(/usr/bin/osascript -e "tell application \"Emacs\"
+        if it is running then
+            return 0
+        else
+            return 1
+        end if
+    end tell")
+    echo "$IS_RUNNING"
+}
 
-# echo "attaching"
-# emacsclient -c "$@" &
-
-# MATCHES="$(ps aux | grep "Emacs -daemon" | wc -l)"
-# # echo "${MATCHES: -1}"
-# case "${MATCHES: -1}" in
-
-# 1)  echo "Mx-start-hacking\n"
-#     cat /Users/mbayer/Settings/dotfiles/lambda.txt
-#     echo "\nStarting server"
-#     emacs -daemon
-#     echo "Attaching"
-#     emacsclient -c "$@" &
-#     ;;
-# 2)  emacsclient "$@" &
-#     ;;
-# esac
-MATCHES="$(ps aux | grep "Emacs -daemon" | wc -l)"
-# echo "${MATCHES: -1}"
-case "${MATCHES: -1}" in
-
-1)  echo "Mx-start-hacking\n"
+if [[ $(emacs_running) == 0 ]]
+then
+    emacsclient "$@" &
+else
+    echo "Mx-start-hacking\n"
     cat /Users/mbayer/Settings/dotfiles/lambda.txt
     echo "\nStarting server"
-    emacs -daemon
+    emacs --daemon
     echo "Attaching"
     emacsclient -c "$@" &
-    ;;
-2)  exec emacsclient --alternate-editor="" "$@" &
-    ;;
-esac
-
-RUNNING=$(/usr/bin/osascript -e "tell application \"Emacs\"
-    if it is running then
-      return \"\n emacs is running\"
-    else
-      return \"not running\"
-    end if
-end tell")
-echo "$RUNNING"
-
-
-# #!/usr/bin/env bash
-
-# # start emacs
-
-# # TODO handle args
-
-# # always use latest brew version
-# emacs_app=/usr/local/bin/emacs
-# # shopt -s nullglob
-# # versions=(/usr/local/Cellar/emacs/*/)
-# # shopt -u nullglob
-# # latest_version=${versions[${#versions[@]}-1]}
-# # emacs_app="$latest_version""Emacs.app"
-
-# if [ ! -x $emacs_app ]; then
-#     echo "Emacs.app not found" >&2
-#     exit 1
-# fi
-
-# # start emacs in background
-# /usr/bin/osascript -e "tell application \"$emacs_app\" to activate" &
-
-# if [ $# -gt 0 ]; then
-#     tempfiles=()
-
-#     while IFS= read -r filename; do
-#         # create tempfiles if neccessary
-#         if [ ! -f "$filename" ]; then
-#             tempfiles+=("$filename")
-#             /usr/bin/touch "$filename"
-#         fi
-
-#         # resolve relative paths
-#         file=$(realpath "$filename")
-#         /usr/bin/osascript -e "tell application \"$emacs_app\" to open POSIX file \"$file\""> /dev/null
-#     done <<< "$(printf '%s\n' "$@")"
-
-#     for tempfile in "${tempfiles[@]}"; do
-#         [ ! -s "$tempfile" ] && /bin/rm "$tempfile"
-#     done
-# fi
+fi
