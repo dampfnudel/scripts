39fc62c1fe258b1247bdc498ea497de066972176
Markus Bayer <markus.bayer@tomcom.de>
2018-04-17 08:40:55 +0200

    working with time regex

--- tampermonkey/TimeInTitle.js
+++ tampermonkey/TimeInTitle.js
@@ -10,10 +10,12 @@
 
 (function() {
   'use strict';
+  var timeRegex = /([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/;
   setInterval(function() {
     document.querySelectorAll('#x-auto-23 > tbody > tr:nth-child(2) > td.x-btn-mc > em > button')[0].click();
     setTimeout(function(){
-      document.title = document.querySelectorAll('#x-auto-75')[0].innerText.substring(21, 26);
+      var hoursStr = document.querySelectorAll('#x-auto-75')[0].innerText;
+      document.title = hoursStr.match(timeRegex)[0];
     }, 2000);
   }, 60 * 1000);
 })();
