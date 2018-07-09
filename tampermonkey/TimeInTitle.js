ad98e4febf1180744754d1d5d6af1e5250947052
Markus Bayer <markus.bayer@tomcom.de>
2018-07-09 11:58:55 +0200

    added doneAt

--- tampermonkey/TimeInTitle.js
+++ tampermonkey/TimeInTitle.js
@@ -8,14 +8,34 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function (workload) {
   'use strict';
-  var timeRegex = /([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/;
-  setInterval(function() {
+  function userInteraction () {
+    var windows = document.querySelectorAll('.x-window');
+    if (windows.length > 0) return true;
+    return false;
+  }
+  function doneAt (currentHours, currentMinutes) {
+    var minutes = currentHours * 60 + currentMinutes;
+    var left = workload - minutes;
+    var d1 = new Date (),
+      d2 = new Date (d1);
+    d2.setMinutes (d1.getMinutes() + left);
+    return d2.toLocaleTimeString();
+  }
+  function time2title () {
+    var timeRegex = /([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/;
+    if (userInteraction()) return;
     document.querySelectorAll('#x-auto-23 > tbody > tr:nth-child(2) > td.x-btn-mc > em > button')[0].click();
-    setTimeout(function(){
+    setTimeout(function () {
       var hoursStr = document.querySelectorAll('#x-auto-75')[0].innerText;
-      document.title = hoursStr.match(timeRegex)[0];
+      var parsed = hoursStr.match(timeRegex)[0];
+      var parts = parsed.split(':');
+      var hours = parseInt(parts[0]);
+      var minutes = parseInt(parts[1]);
+
+      document.title = parsed + ' --|> ' + doneAt(hours, minutes);
     }, 2000);
-  }, 60 * 1000);
-})();
+  }
+  setInterval(time2title, 60 * 1000);
+})(450);
