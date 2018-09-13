55d0747d96ced39cf2cf44f1b84a9fd6e8d777d5
Markus Bayer <markus.bayer@tomcom.de>
2018-09-13 11:21:17 +0200

    updated

--- tampermonkey/prevent-keyboard-hijacking.js
+++ tampermonkey/prevent-keyboard-hijacking.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name           Disable Ctrl+s and Ctrl+t interceptions
+// @name           Disable Cmdl+s and Cmdl+t interceptions
 // @description    Stop websites from highjacking keyboard shortcuts
 //
 // @run-at         document-start
@@ -7,13 +7,12 @@
 // @grant          none
 // ==/UserScript==
 
-// from: https://superuser.com/questions/168087/how-to-forbid-keyboard-shortcut-stealing-by-websites-in-firefox
-// Keycode for 's' and 't'. Add more to disable other ctrl+X interceptions
-keycodes = [83, 84];
+// https://superuser.com/questions/168087/how-to-forbid-keyboard-shortcut-stealing-by-websites-in-firefox
+var keycodes = [83, 84];
 
 (window.opera ? document.body : document).addEventListener('keydown', function(e) {
     // alert(e.keyCode ); //uncomment to find more keyCodes
-    if (keycodes.indexOf(e.keyCode) != -1 && e.ctrlKey) {
+    if (keycodes.indexOf(e.keyCode) != -1 && e.cmdlKey) {
         e.cancelBubble = true;
         e.stopImmediatePropagation();
     // alert("Gotcha!"); //ucomment to check if it's seeing the combo
