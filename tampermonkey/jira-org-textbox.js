4ddbd7891bd8c0a523100fb1d775ff4c80efd82e
Markus Bayer <markus.bayer@tomcom.de>
2019-11-14 12:57:04 +0100

    org links

--- tampermonkey/jira-org-textbox.js
+++ tampermonkey/jira-org-textbox.js
@@ -2,7 +2,7 @@
 // @name         org-ticket-title
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  display a textarea with title, url, description and copy button
+// @description  display a textarea with title, url, description
 // @author       You
 // @match        https://jira.tomcom.de/browse/*
 // @grant        none
@@ -15,13 +15,16 @@
     var refElement = document.getElementById('stalker');
     var button = document.createElement('BUTTON');
 
-
-    var url = document.location.href;
+    var ticketId = document.getElementById('key-val').textContent.trim();
+    var url = document.getElementById('key-val').href;
     var title = document.getElementById('summary-val').textContent.trim();
     var description = document.getElementsByClassName('user-content-block')[0].textContent.trim();
+    // dupes
+    // document.querySelector('.links-container span')
 
     parent.insertBefore(newElement, refElement);
-    newElement.value = '** ' + title + '\n' + url + '\n' + description;
+    // newElement.value = '** ' + ticketId + ' ' + title + '\n' + url + '\n' + description;
+    newElement.value = '** [[' + url + '][' + ticketId + ' ' + title + ']]\n' + description;
     newElement.style['width'] = '850px';
     newElement.setAttribute('rows', 3);
     // newElement.select();
